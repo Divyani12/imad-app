bdb3a02b77981f7b76598692934ebffcb79cffef
Divyani12 <31373908+Divyani12@users.noreply.github.com>
2017-09-02 17:05:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/section1',function(req,res){
-   res.sendFile(path.join('sh.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
 app.get('/section2',function(req,res){
